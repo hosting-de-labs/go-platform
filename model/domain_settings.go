48c87417b3165affb0bf5309be303977ce0f2f77
jean kahrs <j.kahrs@hosting.de>
2020-12-02 12:13:03 +0100

    Revert "sort struct"
    This reverts commit e419bf8f70bfab97896c4d63a961643f49ba902d.

--- model/domain_settings.go
+++ model/domain_settings.go
@@ -1,17 +1,17 @@
 package model
 
 type DomainSettingsObject struct {
-	AddDate                       string `json:"addDate,omitempty"`
 	BundleID                      string `json:"bundleId,omitempty"`
 	DomainName                    string `json:"domainName,omitempty"`
 	DomainNameUnicode             string `json:"domainNameUnicode,omitempty"`
+	StorageQuota                  int    `json:"storageQuota,omitempty"`
+	StorageQuotaAllocated         int    `json:"storageQuotaAllocated,omitempty"`
+	MailboxQuota                  int    `json:"mailboxQuota,omitempty"`
 	ExchangeMailboxQuota          int    `json:"exchangeMailboxQuota,omitempty"`
 	ExchangeStorageQuota          int    `json:"exchangeStorageQuota,omitempty"`
 	ExchangeStorageQuotaAllocated int    `json:"exchangeStorageQuotaAllocated,omitempty"`
+	AddDate                       string `json:"addDate,omitempty"`
 	LastChangeDate                string `json:"lastChangeDate,omitempty"`
-	MailboxQuota                  int    `json:"mailboxQuota,omitempty"`
-	StorageQuota                  int    `json:"storageQuota,omitempty"`
-	StorageQuotaAllocated         int    `json:"storageQuotaAllocated,omitempty"`
 }
 
 type FindDomainSettingsResult struct {
